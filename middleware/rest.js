2f227a2c16188ab1758c95a932cfac2de31b8b0f
inntechy <yinpassing@qq.com>
2018-07-26 03:47:11 +0800

    给ctx绑定rest功能，用于返回data或错误码

--- middleware/rest.js
+++ middleware/rest.js
@@ -1,5 +1,10 @@
-//给context绑定一个rest功能，这样就可以比较方便地调用rest方法来返回API了
 module.exports = {
+    //在controller中，只要try catch即可捕获异常
+    APIError:function (code, message) {
+        this.code = code || 'internal:unknow_error';
+        this.message = message || '';
+    },
+    //给context绑定一个rest功能，这样就可以比较方便地调用rest方法来返回API了
     restify: (pathPrefix) => {
         //REST API前缀 默认为/api/
         pathPrefix = pathPrefix || '/api';
@@ -11,7 +16,18 @@ module.exports = {
                     ctx.response.type = 'application/json';
                     ctx.response.body = data;
                 }
-                await next();
+                try {
+                    await next();
+                }
+                catch (e){
+                    // 返回错误:
+                    ctx.response.status = 400;
+                    ctx.response.type = 'application/json';
+                    ctx.response.body = {
+                        code: e.code || 'internal:unknown_error',
+                        message: e.message || ''
+                    };
+                }
             }
             else {
                 await next();
