cf7816da32b76782738bd454460738e206998adc
inntechy <yinpassing@qq.com>
2018-07-27 16:05:35 +0800

    添加DELETE的api

--- controllers/api.js
+++ controllers/api.js
@@ -10,6 +10,7 @@ module.exports = {
         }else{
             throw new APIError('database:data not found', 'data not found');
         }
+        await next();
     },
     //获取特定ID的送货单
     'GET /api/Inbound_notes/:inid':async (ctx, next) => {
@@ -21,16 +22,38 @@ module.exports = {
         }else{
             throw new APIError('database:data not found', 'data not found');
         }
+        await next();
     },
     //新建一个送货单
-    'PUT /api/Inbound_notes':async (ctx, next) => {
+    'POST /api/Inbound_notes':async (ctx, next) => {
         var data = ctx.request.body;
         if(data != null){
+            var mdate = new Date();
+            data.ID_time = Date.now();
+            data.createAt = mdate.toLocaleString();
+            //console.log(data);
             var return_data = await Inbound_note.create(data);
             ctx.rest(return_data);
-            console.log('created:' + JSON.stringify())
+            console.log('Inbound_notes created');
         }else{
             throw new APIError('database:data is emputy', 'input invalid');
         }
+        await next();
+    },
+    //删除一个送货单
+    'DELETE /api/Inbound_notes/:inid':async (ctx, next) => {
+        var id = ctx.params.inid;
+        if(await Inbound_note.findById(id)){
+            var return_Data = await Inbound_note.destroy({
+                where:{
+                    ID_time:id
+                }
+            })
+            ctx.rest(return_Data);
+            console.log('Inbound_notes deleted');
+        }else{
+            throw new APIError('database:data not found', 'data not found');
+        }
+        
     }
 }
--- 一些设计思路/RESTful API.md
+++ 一些设计思路/RESTful API.md
@@ -12,7 +12,7 @@
 |      |                      |          |          |                |
 |      |                      |          |          |                |
 
-### 1. GET /Inbound_notes
+### 1. GET /api/Inbound_notes
 
 不发送内容，返回一个data对象如下(样例中，有两个Inbound_note)，前端在处理时，应当以ID_time去索引inbound_items，来获得入库单上面的具体货品。
 
@@ -43,7 +43,7 @@
 ]
 ```
 
-###2. GET /Inbound_nots/:inid
+###2. GET /api/Inbound_nots/:inid
 
 不发送内容，:inid用于索引（ID_time == :inid）的入库单，返回一个入库单如下。同样，需要用ID去索引items
 
@@ -61,25 +61,46 @@
 }
 ```
 
-### 3. PUT /Inbound_notes
+### 3. POST /api/Inbound_notes
 
-发送以下信息
+发送以下信息。服务器接收以下信息后，会自动补全ID_time、createAt参数。
 
 ```json
 {
-    "container_id": "柜号",
-    "brand": "品牌",
-    "name": "品名",
-    "level": "等级",
-    "quanlity": 100,
-    "volume_sum": 200.0,
-    "goods_mark": "货号"
+    "container_id": "1",
+    "brand": "4",
+    "name": "4",
+    "level": "4",
+    "quanlity": 7,
+    "volume_sum": 8,
+    "goods_mark": "9"
 }
 ```
 
 应该返回一个与GET /Inbound_notes/:inid相同的数据
 
 ```json
+{
+    "container_id": "1",
+    "brand": "4",
+    "name": "4",
+    "level": "4",
+    "quanlity": 7,
+    "volume_sum": 8,
+    "goods_mark": "9",
+    "ID_time": 1532678322980,
+    "createAt": "2018-7-27 15:58:42"
+}
+```
 
+###4. DELETE /api/Inbound_notes/:inid
+
+若ID正确，将删除数据后返回bool值。若ID错误，会返回如下错误码
+
+```json
+{
+    "code": "database:data not found",
+    "message": "data not found"
+}
 ```
 
