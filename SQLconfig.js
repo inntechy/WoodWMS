ca96200de017a129badbe2c8c3e10019865473ae
inntechy <yinpassing@qq.com>
2018-07-28 18:53:41 +0800

    更新数据库相关配置

--- SQLconfig.js
+++ SQLconfig.js
@@ -1,8 +1,8 @@
 var config = {
     database: 'test', // 使用哪个数据库
     username: 'root', // 用户名
-    password: '1234', // 口令
-    host: '192.168.199.130', // 主机名
+    password: 'dbroot', // 口令
+    host: 'localhost', // 主机名
     port: 3306 // 端口号，MySQL默认3306
 };
 
