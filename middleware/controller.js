6153c6f2f7a3ae44c0f732694797a0485a97b77c
inntechy <yinpassing@qq.com>
2018-07-28 18:58:45 +0800

    添加对PATCH的支持

--- middleware/controller.js
+++ middleware/controller.js
@@ -20,6 +20,10 @@ function addMapping(router, mapping) {
             var path = url.substring(7);
             router.del(path, mapping[url]);
             console.log(`register URL mapping: DELETE ${path}`);
+        } else if (url.startsWith('PATCH ')) {
+            var path = url.substring(6);
+            router.patch(path, mapping[url]);
+            console.log(`register URL mapping: PATCH ${path}`);
         } else {
             console.log(`invalid URL: ${url}`);
         }
